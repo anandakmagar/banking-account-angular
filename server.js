b6bb25183034dbda26f8522957d959e04ea3f49c
Ananda Magar <anandakmagar@gmail.com>
2023-10-02 22:59:38 -0600

    third commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fc-web'));
+app.use(express.static(__dirname + '/dist/banking-angular'));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname+'/dist//index.html'));
+    res.sendFile(path.join(__dirname+'/dist/banking-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
